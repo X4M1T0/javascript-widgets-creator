ee09239024e72714867c7b91a51c0d1984743bc3
X4M1T0 <gabrielsantossousa05@hotmail.com>
2023-02-23 23:23:49 -0300

    0.0

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+/* 
+        VANILLA LOGIN FORM
+*/
+
+function vanillaForm(){
+
+    // VARIABLES
+    var divVanillaForm = document.createElement('div');
+
+    // DIV ATTRIBUTES 
+    divVanillaForm.setAttribute("id", "divVanillaForm");
+    divVanillaForm.setAttribute("class", "formsConfig");
+
+    // DIV ITEMS
+    
+
+
+    document.body.appendChild(divVanillaForm);
+}
